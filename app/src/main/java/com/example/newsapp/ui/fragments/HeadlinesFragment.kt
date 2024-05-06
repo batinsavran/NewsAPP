3795f2c314ef09a6d8e69f105fd3d431ebd72206
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-05-06 20:42:57 +0300

    Added new fragments for bottom navigation menu

---
+++ app/src/main/java/com/example/newsapp/ui/fragments/HeadlinesFragment.kt
@@ -0,0 +1,18 @@
+package com.example.newsapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.newsapp.R
+
+class HeadlinesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_headlines, container, false)
+    }
+}
