7ebd7ffeb17356c97d6f42da9c4c7fdb534409c2
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-05-09 20:04:57 +0300

    Added Models

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "dd506d11006c4d66b0dec0d3e82e945d"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
