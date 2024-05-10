d755081a990fcc5841af93ee4b38fcfe957d74f4
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-05-10 20:19:28 +0300

    Added News View Model

---
+++ app/src/main/java/com/example/newsapp/ui/NewsViewModel.kt
@@ -0,0 +1,124 @@
+package com.example.newsapp.ui
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.newsapp.models.Article
+import com.example.newsapp.models.NewsResponse
+import com.example.newsapp.repository.NewsRepository
+import com.example.newsapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import java.io.IOException
+
+class NewsViewModel(app: Application, val newsRepository: NewsRepository) : AndroidViewModel(app) {
+    var headline: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var headlinesPage = 1
+    var headlinesResponse: NewsResponse? = null
+
+    var searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var searchNewsPage = 1
+    var searchNewsResponse: NewsResponse? = null
+    var newSearchQuery: String? = null
+    var oldSearchQuery: String? = null
+
+    init {
+        getHeadlines("us")
+    }
+
+    fun getHeadlines(countryCode: String) = viewModelScope.launch {
+        headlinesInternet(countryCode)
+    }
+
+    fun searchNews(searchQuery: String) = viewModelScope.launch {
+        searchNewsInternet(searchQuery)
+    }
+
+    private fun handleHeadlinesResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                headlinesPage++
+                if (headlinesResponse == null) {
+                    headlinesResponse = resultResponse
+                } else {
+                    val oldArticles = headlinesResponse!!.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles.addAll(newArticles)
+                }
+                return Resource.Success(headlinesResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+
+    fun addToFavourites(article: Article) = viewModelScope.launch {
+        newsRepository.upsert(article)
+    }
+
+    fun getFavouritesNews() = newsRepository.getFavouriteNews()
+
+    fun deleteToFavourites(article: Article) = viewModelScope.launch {
+        newsRepository.deleteArticle(article)
+    }
+
+    fun internetConnection(context: Context): Boolean {
+        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).apply {
+            return getNetworkCapabilities(activeNetwork)?.run {
+                when {
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    else -> false
+                }
+            } ?: false
+        }
+    }
+
+    private suspend fun headlinesInternet(countryCode: String) {
+        headline.postValue(Resource.Loading())
+        try {
+            if (internetConnection(this.getApplication())!!) {
+                val response = newsRepository.getHeadlines(countryCode, headlinesPage)
+                headline.postValue(handleHeadlinesResponse(response))
+            } else {
+                headline.postValue(Resource.Error("No Internet Connection"))
+            }
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> headline.postValue(Resource.Error("Network Failure"))
+                else -> headline.postValue(Resource.Error("Conversion Error"))
+            }
+        }
+    }
+
+    private suspend fun searchNewsInternet(searchQuery: String) {
+        newSearchQuery = searchQuery
+        searchNews.postValue(Resource.Loading())
+        try {
+            if (internetConnection(this.getApplication())) {
+                val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+                searchNews.postValue(handleHeadlinesResponse(response))
+            } else {
+                searchNews.postValue(Resource.Error("No internet connection"))
+            }
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> searchNews.postValue(Resource.Error("Network Failure"))
+                else -> searchNews.postValue(Resource.Error("Conversion Error"))
+            }
+        }
+    }
+}
+
+private fun Any.searchNews(searchQuery: String, searchNewsPage: Int): Response<NewsResponse> {
+    TODO("Not yet implemented")
+}
+
+private fun <E> List<E>.addAll(newArticles: List<E>) {
+    TODO("Not yet implemented")
+}
